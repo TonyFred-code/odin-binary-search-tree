18ec15468bbe2776f59887b6f9a066ff6a0c2a63
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-22 03:48:32 +0100

    add a pretty print function to visualize tree structure

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,43 +1,58 @@
 class Node {
-    constructor(data ) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class Tree {
+  constructor(array) {
+    this.root = this.#buildTree(array);
+  }
 
-    constructor(array) {
-        this.root = this.#buildTree(array);
-    }
+  #buildTree(array) {
+    if (array.length === 0) return null;
 
-    #buildTree(array) {
-        if (array.length === 0) return null;
+    const mid = Math.floor(array.length / 2);
+    const root = new Node(array[mid]);
 
-        const mid = Math.floor(array.length / 2);
-        const root = new Node(array[mid]);
+    const q = [
+      [root, [0, mid - 1]],
+      [root, [mid + 1, array.length - 1]],
+    ];
 
-        const q = [[root, [0, mid - 1]], [root, [mid + 1, array.length - 1]]];
+    while (q.length > 0) {
+      const [parent, [left, right]] = q.shift();
 
-        while (q.length > 0) {
-            const [parent, [left, right]] = q.shift();
+      if (left <= right && parent !== null) {
+        const mid = Math.floor((left + right) / 2);
+        const child = new Node(array[mid]);
 
-            if (left <= right && parent !== null) {
-                const mid = Math.floor((left + right) / 2);
-                const child = new Node(array[mid]);
-
-                if (array[mid] < parent.data) {
-                    parent.left = child
-                } else {
-                    parent.right = child;
-                }
-
-                q.push([child, [left, mid - 1]]);
-                q.push([child, [mid + 1, right]]);
-            }
+        if (array[mid] < parent.data) {
+          parent.left = child;
+        } else {
+          parent.right = child;
         }
 
-        return root;
+        q.push([child, [left, mid - 1]]);
+        q.push([child, [mid + 1, right]]);
+      }
     }
+
+    return root;
+  }
+
+  prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
 }

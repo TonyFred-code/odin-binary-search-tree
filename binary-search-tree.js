cff24ac2d470db13cbc5d1c7cdd7f5755833fe40
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-22 03:46:06 +0100

    add a buildTree function to construct tree from array

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -5,3 +5,39 @@ class Node {
         this.right = null;
     }
 }
+
+class Tree {
+
+    constructor(array) {
+        this.root = this.#buildTree(array);
+    }
+
+    #buildTree(array) {
+        if (array.length === 0) return null;
+
+        const mid = Math.floor(array.length / 2);
+        const root = new Node(array[mid]);
+
+        const q = [[root, [0, mid - 1]], [root, [mid + 1, array.length - 1]]];
+
+        while (q.length > 0) {
+            const [parent, [left, right]] = q.shift();
+
+            if (left <= right && parent !== null) {
+                const mid = Math.floor((left + right) / 2);
+                const child = new Node(array[mid]);
+
+                if (array[mid] < parent.data) {
+                    parent.left = child
+                } else {
+                    parent.right = child;
+                }
+
+                q.push([child, [left, mid - 1]]);
+                q.push([child, [mid + 1, right]]);
+            }
+        }
+
+        return root;
+    }
+}

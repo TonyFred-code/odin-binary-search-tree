7ff606ab32385f0db809a4b11a19fda9d3f3d5f5
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-23 02:18:30 +0100

    add method for deleting a node recursively in the all possible cases

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -103,26 +103,84 @@ class Tree {
 
   insertRecursive(value) {
     this.#insertRecursiveHelper(this.root, value);
-}
+  }
 
-#insertRecursiveHelper(root, value) {
+  #insertRecursiveHelper(root, value) {
     if (root === null) {
-        root = new Node(value);
-        return root;
+      root = new Node(value);
+      return root;
     }
 
     if (value < root.data) {
-        root.left = this.#insertRecursiveHelper(root.left, value);
+      root.left = this.#insertRecursiveHelper(root.left, value);
     } else if (value > root.data) {
-        root.right = this.#insertRecursiveHelper(root.right, value);
+      root.right = this.#insertRecursiveHelper(root.right, value);
     }
 
     return root;
-}
+  }
+
+  deleteRecursive(value) {
+    this.#deleteRecursiveHelper(this.root, value);
+  }
+
+  #deleteRecursiveHelper(root, value) {
+    if (root === null) {
+      return root;
+    }
+
+    if (root.data > value) {
+      root.left = this.#deleteRecursiveHelper(root.left, value);
+      return root;
+    } else if (root.data < value) {
+      root.right = this.#deleteRecursiveHelper(root.right, value);
+      return root;
+    }
+
+    if (root.left === null) {
+      return root.right;
+    } else if (root.right === null) {
+      return root.left;
+    } else {
+      let parent = root;
+
+      let successor = root.right;
+      while (successor.left !== null) {
+        parent = successor;
+        successor = successor.left;
+      }
+
+      if (parent !== root) {
+        parent.left = successor.right;
+      } else {
+        parent.right = successor.right;
+      }
+
+      root.data = successor.data;
+
+      return root;
+    }
+  }
 }
 
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const BST = new Tree(array);
 // BST.insertIterative(2);
 BST.insertRecursive(2);
+console.log('tree before deletion');
+BST.prettyPrint(BST.root);
+console.log('delete 2 - a leaf node');
+BST.deleteRecursive(2);
+console.log('delete 7 - another leaf node');
+BST.deleteRecursive(7);
+// console.log('delete a node with one child - left');
+// BST.deleteRecursive(3)
+// console.log('delete a node with one child - right');
+// BST.deleteRecursive(5)
+// console.log('delete a node with two children - 67');
+// BST.deleteRecursive(67);
+// console.log('delete a node with two children - 4');
+// BST.deleteRecursive(4);
+
+console.log('tree after deletion');
 BST.prettyPrint(BST.root);
